8523091c4ca4d894b19c54692843e18cd76385ae
ortodox70 <novozheev1970@mail.ru>
2024-06-15 20:15:04 +1000

    Add files via upload

---
+++ UROK 2.py
@@ -0,0 +1,25 @@
+d = "весело живем"
+print(len(d))
+first = 6
+second = 4
+print ( first + second )
+print ( first - second )
+sum = first + second
+diff = first - second
+print (sum)
+print (diff)
+first = 5
+second = 7
+third = 9
+mean = (first + second + third) /3
+print(mean)
+first_string = "вторник"
+second_string = "понедельник"
+print(second_string +", "  + first_string)
+a = 3
+b = 5
+c = 7
+f = (a * b) + (a * c)
+f**3/2
+print (f**3/2)
+
